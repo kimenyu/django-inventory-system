90b1acb48464ee1a88a7efe7d6925e7684cd99cb
kimenyu <njorogekimenyu@gmail.com>
2025-05-14 12:48:03 +0300

    adding product and category serializers

--- core/Inventory/serializers.py
+++ core/Inventory/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from .models import Category, Product, Warehouse, Batch, SalesOrder, SalesOrderItem, StockIn, StockOut, Supplier, Customer, PurchaseOrder, PurchaseOrderItem, Inventory, AuditLog
+from typing import Any, Dict, List
 
-  
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['sku', 'name', 'description', 'price', 'unit']
-        
-        
-    def validate_price(self, value):
+
+    def validate_price(self, value: float) -> float:
         if value < 0:
             raise serializers.ValidationError("Price cannot be less than 0")
         return value
-            
+
+
 class CategorySerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True)
 
@@ -21,15 +22,15 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['name', 'description', 'products']
 
-    def create(self, validated_data):
-        products_data = validated_data.pop('products')
+    def create(self, validated_data: Dict[str, Any]) -> Category:
+        products_data: List[Dict[str, Any]] = validated_data.pop('products')
         category = Category.objects.create(**validated_data)
         for product_data in products_data:
             Product.objects.create(category=category, **product_data)
         return category
 
-    def update(self, instance, validated_data):
-        products_data = validated_data.pop('products', [])
+    def update(self, instance: Category, validated_data: Dict[str, Any]) -> Category:
+        products_data: List[Dict[str, Any]] = validated_data.pop('products', [])
 
         # Update category fields
         instance.name = validated_data.get('name', instance.name)
